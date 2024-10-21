2c5c8619259e59611360f82f0466361293f81e42
Sinalo Xaba-221759883 <221759883@edu.vut.ac.za>
2024-10-21 20:39:18 +0200

    Add files via upload

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,4 +1,5 @@
 # prediction_model.py
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
@@ -28,3 +29,7 @@ print(f"Mean Absolute Error: {mae}")
 # Display predictions and actual values
 predicted_vs_actual = pd.DataFrame({'Predicted': y_pred, 'Actual': y_test})
 print(predicted_vs_actual.head())
+
+
+def generate_data():
+    return None
